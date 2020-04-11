128ed2052af365a6c0df81238066d637cb359ba1
Rafael Basask <basask@localhost.localdomain>
2020-04-11 10:30:33 -0300

    Task Allocation first version - Simple task allocation

---
+++ precache-manifest.59b14ca8c2f436837a54a036e09dd38d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685c06d9ed7117938aeea4863b6a2c48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d2cafbc6dea0eaf052d",
+    "url": "/static/css/2.caf5aa68.chunk.css"
+  },
+  {
+    "revision": "7f78c9ce6c27813ed6e6",
+    "url": "/static/css/main.a4357624.chunk.css"
+  },
+  {
+    "revision": "1d2cafbc6dea0eaf052d",
+    "url": "/static/js/2.84fb2c2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84fb2c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f78c9ce6c27813ed6e6",
+    "url": "/static/js/main.3addcd5a.chunk.js"
+  },
+  {
+    "revision": "07cd8e6c7bf326ac1dfa",
+    "url": "/static/js/runtime-main.d3dc58a1.js"
+  }
+]);
